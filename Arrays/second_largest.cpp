b1f0ee16709b236669da25167bc05019b1ac0b2c
Dhanusha B H <dhanushabh3@gmail.com>
2026-01-31 10:41:57 +0530

    AddSecond largest element in array

---
+++ Arrays/second_largest.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int secondlargest(vector<int>&arr,int n){
+    int largest=arr[0];
+    int slargest=-1;
+    for(int i=1;i<n;i++){
+        if(arr[i]>largest){
+            slargest=largest;
+            largest=arr[i];
+        }
+        else if(arr[i]<largest&&arr[i]>slargest){
+            slargest=arr[i];
+
+
+        }
+    }
+    return slargest;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int result=secondlargest(arr,n);
+    cout<<result;
+}
