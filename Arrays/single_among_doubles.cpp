170ec44f0c72487874066c5b09df3d59bc512499
Dhanusha B H <dhanushabh3@gmail.com>
2026-02-01 15:01:35 +0530

    Find the number that appears once & the others twice

---
+++ Arrays/single_among_doubles.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int getsingleelement(int arr[],int n){
+    int xorr=0;
+    for(int i=0;i<n;i++){
+        xorr^=arr[i];
+    }
+    return xorr;
+
+
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int result=getsingleelement(arr,n);
+    cout<<result;
+}
