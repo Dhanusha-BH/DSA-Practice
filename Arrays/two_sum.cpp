2e9d8ab50406a8f85d6be56211870bb4ac932034
Dhanusha B H <dhanushabh3@gmail.com>
2026-02-01 21:55:30 +0530

    Two sum

--- Arrays/two_sum.cpp
+++ Arrays/two_sum.cpp
@@ -1,22 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
+string twosum(vector<int>&arr,int n,int target){
+    int left=0,right=n-1;
+    sort(arr.begin(),arr.end());
+    while(left<right){
+        int sum=arr[left]+arr[right];
+        if(sum==target){
+            return "Yes";
+        }
+        else if(sum<target)left++;
+        else right--;
+    }
+    return "No";
+
+}
 
 int main() {
-    vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
-
-    unordered_map<int, int> mp;
-
-    for (int i = 0; i < nums.size(); i++) {
-        int need = target - nums[i];
-
-        if (mp.find(need) != mp.end()) {
-            cout << mp[need] << " " << i << endl;
-            return 0;
-        }
-
-        mp[nums[i]] = i;
-    }
-
-    return 0;
-}
+   int n;
+   cin>>n;
+   vector<int>arr(n);
+   for(int i=0;i<n;i++){
+    cin>>arr[i];
+   }
+   int target;
+   cin>>target;
+    string result=twosum(arr,n,target);
+    cout<<result;
+   }
