da8d1682b7be68f57291b66ad5c308e4a3d64b76
Dhanusha B H <dhanushabh3@gmail.com>
2026-03-23 19:33:58 +0530

    Add Search Insert Position

---
+++ BinarySearch/search_insert_position.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int searchinsertposition(int arr[],int n,int target){
+    int low=0,high=n-1;
+    int ans=n;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]>=target){
+            ans=mid;
+            high=mid-1;
+        }else{
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
+    int result=searchinsertposition(arr,n,target);
+    cout<<result;
+}
